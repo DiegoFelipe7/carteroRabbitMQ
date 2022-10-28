14187bcbc66d9591f6c4cf991f3ff0a704389123
SDiegoFelipe7 <diegofelipem99@hotmail.com>
2022-10-28 17:16:24 -0500

    Problema del cartero

---
+++ src/main/java/com/sofka/carteroRabbitMQ/CarteroRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.carteroRabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteroRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteroRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/carteroRabbitMQ/CarteroRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.carteroRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteroRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
